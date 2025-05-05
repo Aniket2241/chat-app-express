c99fbab316aa1a1b8b0426cb853691939f4d1482
Aniket2241 <sprateek507@gmail.com>
2025-05-05 14:50:21 +0530

    production changes made

--- server/index.js
+++ server/index.js
@@ -79,4 +79,5 @@ app.use("/", (req, res) => {
 });
 
 
-server.listen(5001, () => console.log("Server is running"));
+const PORT = process.env.PORT || 5001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
